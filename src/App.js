e523c0003fe8abe07f112689c60c89f82da221bd
endorphin82 <endorphin82@gmail.com>
2021-08-08 18:32:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [logIn, setLogIn] = useState({username: "", password: ""});
+    const [XCSRFToken, setXCSRFToken] = useState({});
+    console.log(XCSRFToken)
+    const handleChangeLogIn = (e) => {
+        const {name, value} = e.target
+        setLogIn({...logIn, [name]: value})
+    }
+    const handleOnClick = async () => {
+
+        try {
+            const tok = await axios({
+                    method: "POST",
+                    url: "https://agile-garden-50413.herokuapp.com/api/token/login/",
+                    headers: {
+                        "X-CSRFToken": "GoSFgMx2Ke0sZerQhShybL8c5E0MX4FIdtvr1U1VrFAqFjcQBg1tnTLAt0VscWPt",
+                        "Content-Type": "application/json"
+                    },
+                    data: {
+                        ...logIn
+                    }
+                }
+            )
+            setXCSRFToken(tok)
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+    return (
+        <div className="App">
+            <label>username</label>
+            <input value={String(logIn.username)} type="text" name="username" onChange={handleChangeLogIn}/>
+            <br/>
+            <label>password</label>
+            <input value={String(logIn.password)} type="password" name="password" onChange={handleChangeLogIn}/>
+            <br/>
+            <button onClick={handleOnClick}>submit</button>
+        </div>
+    );
 }
 
 export default App;
